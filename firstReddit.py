745a8e0a55ddffda40c87712ce0653e33a1e5ab8
Skyler Ackerman <stackerm@ucsd.edu>
2021-07-01 22:00:29 -0700

    Fixing up the code

--- firstReddit.py
+++ firstReddit.py
@@ -1,23 +1,25 @@
-import praw, requests, urllib, os
+import praw, requests, os
 
+# If you don't want to have the variables stored in the 
 reddit = praw.Reddit(
     client_id=os.environ['REDDIT_ID'],
     client_secret=os.environ['REDDIT_SECRET'],
-    user_agent="aUCSDStud3nt",
-    username="aUCSDStud3nt",
+    user_agent="<YOUR-USERNAME>",
+    username="<YOUR-USERNAME>",
     password=os.environ['REDDIT_PWD']
 )
 
+accepted_contents = {'image/jpeg':'.jpg', 'image/png':'.png'}
+
+# Change this to whatever folder you have for your lockscreen
 FILEPATH = '../../../Pictures/LockScreen/'
-for submission in reddit.subreddit("Wallpaper").hot(limit=20):
-	try:
-			if '.jpg' in submission.url:
-				with open(FILEPATH + submission.title +'.jpg', 'wb') as f:
-					pic = requests.get(submission.url)
-					f.write(pic.content)
-			if '.png' in submission.url:
-				with open(FILEPATH + submission.title +'.png', 'wb') as f:
-					pic = requests.get(submission.url)
-					f.write(pic.content)
-	except:
-			continue
+
+# Use whatever reddit you want, I used r/Wallpaper
+for submission in reddit.subreddit("Wallpaper").hot(limit=10):
+	# Get the "picture"
+	pic = requests.get(submission.url)
+	# Check if the content type is a jpeg or png
+	if pic.headers['Content-Type'] in accepted_contents.keys():
+		# Save the image
+		with open(FILEPATH + submission.title + accepted_contents[pic.headers['Content-Type']], 'wb') as f:
+			f.write(pic.content)
